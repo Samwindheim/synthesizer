1329b98be8bbe1c70b9cb220a3fc7c8682c7183b
Sam Windheim <sam@windheim.org>
2023-03-09 22:21:29 -0800

    has some functionality to change attack and decay

--- synth.js
+++ synth.js
@@ -6,7 +6,8 @@ const volume = context.createGain();
 volume.connect(context.destination);
 
 const volumeControl = document.querySelector('#volume-control')
-volume.gain.value = .08;
+const release = document.querySelector('#release')
+volume.gain.value = .1;
 
 volumeControl.addEventListener('input', changeVolume);
 
@@ -30,6 +31,7 @@ const c5_Button = document.querySelector("#C5")
 
 const noteButtons = [c4_Button, cSharp_Button, d_Button, dSharp_Button, e_Button, f_Button, fSharp_Button, g_Button, gSharp_Button, a_Button, aSharp_Button, b_Button, c5_Button]
 
+// notes and their associated frequencies
 const Notes = [
     { name: "C4", frequency: 261.63 }, // C4
     { name: "C#", frequency: 277.18 }, 
@@ -46,13 +48,27 @@ const Notes = [
     { name: "C5", frequency: 523.25 }, // C5
 ]
 
+// synth functionality
 for (let i = 0; i <= noteButtons.length; i++) {
     noteButtons[i].addEventListener('click', function() {
         const oscillator = context.createOscillator();
-        oscillator.frequency.setValueAtTime(Notes[i].frequency, context.currentTime);
-        oscillator.connect(volume);
-        oscillator.start(0);
-        oscillator.stop(context.currentTime + 1)
+        const noteGain = context.createGain();
+        noteGain.gain.value = .1
+        //noteGain.gain.setValueAtTime(volume.gain.value, context.currentTime)
+        //noteGain.gain.setValueAtTime(volume.gain.value, currentTime + .3);
+        // noteGain.gain.linearRampToValueAtTime(.2, context.currentTime + .3);
+        
         oscillator.type = 'triangle';
+        oscillator.frequency.setValueAtTime(Notes[i].frequency, context.currentTime); 
+
+        noteGain.gain.linearRampToValueAtTime(.1, context.currentTime + 1 ) // attack
+
+        // approaches the gain value at first parameter by second parameter's time
+        noteGain.gain.linearRampToValueAtTime(.0001, context.currentTime + 4 - .9) // decay
+        
+        oscillator.start(0);
+        oscillator.stop(context.currentTime + 4)
+        oscillator.connect(noteGain);
+        noteGain.connect(volume);
         });
 }
